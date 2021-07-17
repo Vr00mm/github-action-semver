1f3e4b516512ab6ec38a8ece0111518addf0e4f2
Remi ZIOLKOWSKI <remi.ziolkowski@gmail.com>
2021-07-17 20:52:13 +0000

    master IS master
    Signed-off-by: Remi ZIOLKOWSKI <remi.ziolkowski@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ echo here we go
 
 [ "${CURRENT_VERSION}" == "" ] && git-semv minor --url "$GITHUB_ACTION_REPOSITORY"  && exit 0
 
-[[ "$BRANCH_NAME" == "main" ]] && git-semv minor --url "$GITHUB_ACTION_REPOSITORY"
+[[ "$BRANCH_NAME" == "master" ]] && git-semv minor --url "$GITHUB_ACTION_REPOSITORY"
 [[ "$BRANCH_NAME" == "develop" ]] && git-semv minor --url "$GITHUB_ACTION_REPOSITORY" --pre-name dev
 [[ "$BRANCH_NAME" == "feature"* ]] && git-semv minor --url "$GITHUB_ACTION_REPOSITORY" --pre-name feature
 [[ "$BRANCH_NAME" == "hotfix"* ]] && echo -n $(git-semv now --url "$GITHUB_ACTION_REPOSITORY")-hotfix
