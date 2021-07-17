e9e3530270e332b632f0379221dda2e402d52409
BuildTools <unconfigured@null.spigotmc.org>
2021-07-17 12:04:20 +0000

    semv

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM alpine:3.10
+FROM alpine:3.14
+
+RUN apk update && apk add git
 
 COPY LICENSE README.md /
-
+COPY git-semv /usr/local/bin
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,24 @@
 #!/bin/sh -l
 
-echo "hello $1"
+[ -z ${1+x} ] && echo "You should provide the branch name as argument" && exit 1
+BRANCH_NAME="${1}"
+
+printenv
+ls -lah
+pwd
+
+[ ! -d "/repo" ] && echo "The git repository should be available at /repo" && exit 1
+cd /repo
+
+ls -lah
+
+echo "get current version"
+CURRENT_VERSION=`git-semv now 2>/dev/null`
+
+echo "check current version not empty"
+[ "${CURRENT_VERSION}" == "" ] && git-semv minor && exit 0
+
+[[ "$BRANCH_NAME" == "master" ]] && git-semv minor
+[[ "$BRANCH_NAME" == "develop" ]] && git-semv minor --pre-name dev
+[[ "$BRANCH_NAME" == "feature"* ]] && git-semv minor --pre-name feature
+[[ "$BRANCH_NAME" == "hotfix"* ]] && echo -n $(git semv now)-hotfix
