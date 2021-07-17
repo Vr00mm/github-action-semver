75b82e1e7ba288c0d6355d6e5bc53f1aa79af635
Remi ZIOLKOWSKI <remi.ziolkowski@gmail.com>
2021-07-17 21:00:11 +0000

    Fix env var
    Signed-off-by: Remi ZIOLKOWSKI <remi.ziolkowski@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -lx
 
 BRANCH_NAME=${GITHUB_REF##refs/heads/}
-CURRENT_VERSION=`git-semv now --url "$GITHUB_ACTION_REPOSITORY" 2>/dev/null`
-[ "${CURRENT_VERSION}" == "" ] && git-semv minor --url "$GITHUB_ACTION_REPOSITORY"  && exit 0
+CURRENT_VERSION=`git-semv now --url "$GITHUB_REPOSITORY" 2>/dev/null`
+[ "${CURRENT_VERSION}" == "" ] && git-semv minor --url "$GITHUB_REPOSITORY"  && exit 0
 
-[[ "$BRANCH_NAME" == "master" ]] && git-semv minor --url "$GITHUB_ACTION_REPOSITORY"
-[[ "$BRANCH_NAME" == "develop" ]] && git-semv minor --url "$GITHUB_ACTION_REPOSITORY" --pre-name dev
-[[ "$BRANCH_NAME" == "feature"* ]] && git-semv minor --url "$GITHUB_ACTION_REPOSITORY" --pre-name feature
-[[ "$BRANCH_NAME" == "hotfix"* ]] && echo -n $(git-semv now --url "$GITHUB_ACTION_REPOSITORY")-hotfix
+[[ "$BRANCH_NAME" == "master" ]] && git-semv minor --url "$GITHUB_REPOSITORY"
+[[ "$BRANCH_NAME" == "develop" ]] && git-semv minor --url "$GITHUB_REPOSITORY" --pre-name dev
+[[ "$BRANCH_NAME" == "feature"* ]] && git-semv minor --url "$GITHUB_REPOSITORY" --pre-name feature
+[[ "$BRANCH_NAME" == "hotfix"* ]] && echo -n $(git-semv now --url "$GITHUB_REPOSITORY")-hotfix
 exit 0
