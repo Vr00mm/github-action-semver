56690b4d088127ae54f0b6f93ed4e2e6b61d9fdc
Remi ZIOLKOWSKI <remi.ziolkowski@gmail.com>
2021-07-17 20:24:26 +0000

    Release
    Signed-off-by: Remi ZIOLKOWSKI <remi.ziolkowski@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM alpine:3.14
 
-RUN apk update && apk add git tree && mkdir --parent /github/workspace
+RUN apk update && apk add git tree curl && mkdir /github
 
 COPY LICENSE README.md /
-COPY git-semv /usr/local/bin
 COPY entrypoint.sh /entrypoint.sh
 
+RUN curl -sSL --output git-semv.tgz https://github.com/Vr00mm/git-semv/releases/download/v1.1.2/git-semv_linux_x86_64.tar.gz && tar xzvf git-semv.tgz && ls && chmod +x git-semv && mv git-semv /usr/local/bin
 WORKDIR /github/workspace
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/sh -l
 
-pwd
-tree /github
-
 BRANCH_NAME=${GITHUB_REF##refs/heads/}
 
-echo "get current version"
 CURRENT_VERSION=`git-semv now 2>/dev/null`
 
-echo "check current version not empty"
 [ "${CURRENT_VERSION}" == "" ] && git-semv minor && exit 0
 
-[[ "$BRANCH_NAME" == "master" ]] && git-semv minor
+[[ "$BRANCH_NAME" == "main" ]] && git-semv minor
 [[ "$BRANCH_NAME" == "develop" ]] && git-semv minor --pre-name dev
 [[ "$BRANCH_NAME" == "feature"* ]] && git-semv minor --pre-name feature
 [[ "$BRANCH_NAME" == "hotfix"* ]] && echo -n $(git semv now)-hotfix
